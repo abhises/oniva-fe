4fa7abec7b4823fd4c7ef401990b016709660f26
abhises <abhisespoudyal@gmail.com>
2026-03-30 16:50:31 +0545

    adding and fixing the bug of use client

--- app/[locale]/admin/dashboard/page.tsx
+++ app/[locale]/admin/dashboard/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState, useMemo, use } from "react";
 import { useTranslation } from "react-i18next";
 import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
